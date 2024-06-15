e8eac4dc2e2d2e07a5b50b09a33467ad2f431d4b
Fakhri Rossi <rossifakhri@gmail.com>
2024-06-16 06:32:15 +0700

    Write my own middleware

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const morgan = require('morgan');
 // app.use(morgan('tiny'));
 app.use(morgan('dev'));
 
+app.use((req, res, next) => {
+    console.log('Midlleware pertama');
+    next();
+    console.log('Midlleware setelah pertama');
+});
+
+app.use((req, res, next) => {
+    console.log('Midlleware Kedua');
+    next();
+});
+
 app.get('/', (req, res) => {
    res.send('Hello, World!');
 });
